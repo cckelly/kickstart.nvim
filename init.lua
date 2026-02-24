c8233c7f4e9006aec051fa30d422ab2590c954e8
cckelly <cckelly21@gmail.com>
2026-02-24 12:41:07 -0500

    Moved colorscheme and catppuccin setup to its own custom plugin file

--- init.lua
+++ init.lua
@@ -794,24 +794,6 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    opts = {
-      flavour = 'mocha',
-      no_italic = true,
-    },
-    config = function(_, opts)
-      require('catppuccin').setup(opts)
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+-- You can easily change to a different colorscheme.
+-- Change the name of the colorscheme plugin below, and then
+-- change the command in the config to whatever the name of that colorscheme is.
+--
+-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  opts = {
+    flavour = 'mocha',
+    no_italic = true,
+  },
+  config = function(_, opts)
+    require('catppuccin').setup(opts)
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+}
