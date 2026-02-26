91217b1a8ac2ed1ff8f2b210626afb4d9c80fee8
cckelly <cckelly21@gmail.com>
2026-02-25 20:26:48 -0500

    Cleaned up init.lua kickstart comments and made yap highlight color mauve from catppuccin

--- init.lua
+++ init.lua
@@ -7,13 +7,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
+-- [[ Setup custom settings ]]
 require 'custom.options'
-
--- [[ Basic Keymaps ]]
 require 'custom.keymaps'
-
--- [[ Basic Autocommands ]]
 require 'custom.autocommands'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
@@ -39,22 +35,7 @@ rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
@@ -65,23 +46,6 @@ require('lazy').setup({
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
+    icons = {},
   },
 })
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -7,5 +7,9 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function() vim.hl.on_yank() end,
+  callback = function()
+    local colors = require('catppuccin.palettes').get_palette 'mocha'
+    vim.api.nvim_set_hl(0, 'YankHighlight', { bg = colors.mauve, fg = colors.base })
+    vim.hl.on_yank { higroup = 'YankHighlight', timeout = 200 }
+  end,
 })
